76eccb5905438c2680f1b597dc9c11c3e4e205f3
toddmac12 <toddmac12@gmail.com>
2021-08-27 16:57:44 -0400

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
-function PizzaParlor(topping, size, cost){
-    this.topping = topping;
-    this.size = size;
-    this.cost = cost;
-    }
-    
- PizzaParlor.prototype.orderPizza = function(){};
- PizzaParlor.prototype.costOfOrder = function(){};
+//Business Logic
+
+function PizzaParlor(topping, size, cost) {
+  this.topping = topping;
+  this.topping2 = topping2
+  this.size = size;
+  this.cost = 0;
+}
+
+PizzaParlor.prototype.orderPizza = function () { };
+PizzaParlor.prototype.costOfOrder = function () { };
+
+//User Interface
+$(document).ready(function ()
